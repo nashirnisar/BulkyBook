6d76168f8b99252d9b698a76a931722572435183
Nashir <nashirnisar@gmail.com>
2023-06-12 14:13:16 +0530

    Add project files.

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [DisplayName ("Display Order")]
+        [Range(1,100, ErrorMessage="Display Order Must Be 1 to 100 Only")]
+        
+        public int DisplayOrder { get; set; } 
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
